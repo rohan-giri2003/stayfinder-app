de2fa6eae4df4dbc43e4af899c3ddedbeeff8a30
rohan-giri2003 <rohangiri2003@gmail.com>
2026-07-22 16:09:44 +0530

    Use exact firebase config from console

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAKtWajzDGCmAaV3tZ-a9WvMKUx_JPJ-1U",
+  authDomain: "stayfinder-app-928a7.firebaseapp.com",
+  projectId: "stayfinder-app-928a7",
+  storageBucket: "stayfinder-app-928a7.appspot.com",
+  messagingSenderId: "750943602429",
+  appId: "1:750943602429:web:581da8163d7c60e63d2094"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
